58796938060bef999a3a7497b95a486c8e1ec8da
webNekit <mochalov-hunter@mail.ru>
2024-02-27 13:28:06 +0300

    Доделали компонент Hero

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -18,19 +18,17 @@ function Hero() {
             </div>
 
             <div className="lg:py-24">
-              <h2 className="text-3xl font-bold sm:text-4xl">Grow your audience</h2>
+              <h2 className="text-3xl font-bold sm:text-4xl">Добро пожаловать в нашу клинику</h2>
 
               <p className="mt-4 text-gray-600">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-                eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
-                quidem quam repellat.
+                Наши специалисты всегда готовы помочь Вам обрести здоровье!
               </p>
 
               <a
                 href="#"
                 className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
               >
-                Get Started Today
+                Get Started
               </a>
             </div>
           </div>
