e2627021e7c9e11a065cfe1431515ee20dcb10dc
webNekit <mochalov-hunter@mail.ru>
2024-03-02 11:07:12 +0300

    Создали часть Backend на Strapi

--- frontend/src/components/CategorySearch/CategorySearch.jsx
+++ frontend/src/components/CategorySearch/CategorySearch.jsx
@@ -7,7 +7,7 @@ function CategorySearch() {
   return (
     <div className="mb-10 flex items-center flex-col gap-4">
         <h2 className="font-bold text-4xl tracking-wide">Поиск врача</h2>
-        <h3 className="text-gray-400 text-xl">Вы можете найти любого врача, чтобы он вылечил Вас!</h3>
+        <h3 className="text-gray-400 text-xl text-center">Вы можете найти любого врача, чтобы он вылечил Вас!</h3>
         <div className="flex w-full max-w-sm items-center space-x-2">
             <Input type="email" placeholder="Петров Владимир Юрьевич" />
             <Button type="submit" className="flex items-center gap-2">
