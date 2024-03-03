166f794b300daefc7bbc18fca467a6f083326b8f
webNekit <mochalov-hunter@mail.ru>
2024-03-03 21:35:23 +0300

    Сделали эффект Skelton

--- frontend/src/components/DoctorList/DoctorList.jsx
+++ frontend/src/components/DoctorList/DoctorList.jsx
@@ -7,12 +7,17 @@ function DoctorList({doctorsList}) {
         <div className="w-full flex flex-col items-center gap-6">
             <h2 className="text-center font-bold text-xl">Наша гордость!</h2>
             <div className="w-full grid grid-cols-3 gap-4">
-                {doctorsList&&doctorsList.map((item, index) => (
+                {doctorsList.length>0?doctorsList.map((item, index) => (
                     <div key={index} className="border rounded-md border-blue-100 p-3">
                         <Image src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes?.Image?.data.attributes?.url}`} className="w-full h-[270px] object-cover rounded-md" alt={item.attributes?.Name} width={500} height={250} />
                         <h3 className="mt-4 text-xl font-semibold">{item.attributes?.Name}</h3>
                     </div>
-                ))}
+                )) 
+                :
+                [1,2,3,4,5,6].map((item,index) => (
+                    <div className="bg-slate-100 h-[340px] w-full rounded-md animate-pulse"></div>
+                ))
+            }
             </div>
         </div>
     </section>
